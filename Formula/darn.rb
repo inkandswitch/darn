31f140c4afb9b23e135ecd935749082763301c36
Brooklyn Zelenka <hello@brooklynzelenka.com>
2026-03-11 00:25:43 -0700

    Bump brew formula

--- Formula/darn.rb
+++ Formula/darn.rb
@@ -9,23 +9,23 @@
 class Darn < Formula
   desc "CLI for CRDT-backed file sync with automatic conflict resolution"
   homepage "https://github.com/inkandswitch/darn"
-  version "0.4.0"
+  version "0.5.0"
   license any_of: ["Apache-2.0", "MIT"]
 
   on_macos do
     url "https://github.com/inkandswitch/darn/releases/download/v#{version}/darn-macos-aarch64-#{version}"
-    sha256 "ef90c82c3440689f8b75682a5c7116d7194629d08e229d35664a2ea91644361f"
+    sha256 "a5df9fe868f1d81a80ad092c0cf4fd112952a63022a9e42a6a7ce28f35d74415"
   end
 
   on_linux do
     on_arm do
       url "https://github.com/inkandswitch/darn/releases/download/v#{version}/darn-linux-aarch64-musl-#{version}"
-      sha256 "7fb4d57f51cf0427e8845df3b6b97f904f4be1a94c62443c46d23ecec40358c2"
+      sha256 "74c5d5d61690792852b2deeb164adcbc6f7a62f9b943cc7f6b839813791b12ce"
     end
 
     on_intel do
       url "https://github.com/inkandswitch/darn/releases/download/v#{version}/darn-linux-x86_64-musl-#{version}"
-      sha256 "8e873bbe26ca9b6ca08602381e52e7d4dbeb6d56da16c79341fefa1c0932c628"
+      sha256 "30359c1217de49557d44d0df70e088855e8b8f748011db0f9e08247d4e1d7c65"
     end
   end
 
